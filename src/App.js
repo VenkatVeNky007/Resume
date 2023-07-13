bde9c9e92c4ceabb5d4dce0c3e064755fde8abb5
venkat sai <venky6572@gmail.com>
2023-07-13 11:26:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className='body' >
+        <h1 className='res'>RESUME</h1>
  <aside>
         
-        <h1 id="nam">P VENKAT SAI</h1>
+        <h1 id="nam">PITHANI VENKATA SAI BABA</h1>
          
-    <i class="material-symbols-outlined md2 ">mail</i><a href="mailto:venky6572@gmail.com;" title="click to sent mail">venky6572@gmail.com</a> <br/>
+    <i class="material-symbols-outlined md2 ">mail</i><a href="mailto:venky6572@gmail.com;" className='mail' title="click to sent mail">venky6572@gmail.com</a> <br/>
     <i class="material-symbols-outlined md2 ">call</i><a href="tel:7569822741;" title="click to call">7569822741</a>
     <br/><br/><br/><i class="material-symbols-outlined md2">calendar_month</i><span class="xcv">DOB: 16<sup>th</sup> JUL 2000</span>
     <br/><br/><br/><i class="material-symbols-outlined md2">sentiment_satisfied</i><span class="xcv">Unmarried</span>
@@ -31,21 +32,25 @@ function App() {
                 <th>QUALIFICATION</th>
                 <th>INSTITUTION</th>
                 <th>PERCENTAGE</th>
+                
             </tr>
             <tr>
                 <td>B COM</td>
                 <td>Ambedkar open university</td>
-                <td>65%</td>
+                <td>Pursuing</td>
+                
             </tr>
             <tr>
                 <td>INTER</td>
                 <td>Telangana open school society</td>
                 <td>70%</td>
+                
             </tr>
             <tr>
                 <td>SSC</td>
                 <td>POORNIMA VIDYA NIKETAN HIGH SCHOOL</td>
                 <td>82%</td>
+                
             </tr>
         </table>    </fieldset><br/>
         <fieldset>
@@ -59,12 +64,22 @@ function App() {
                 <li>Interested in Reading News & All Other Entertainment Shows</li>
             </ul>
         </fieldset><br/>
+
+        <fieldset>
+            <legend>Hobbies:</legend>
+            <ul>
+                <li>Playing Cricket</li>
+                <li>Watching TV</li>
+                
+                
+            </ul>
+        </fieldset><br/>
         
         <fieldset>
             <legend>DECLARATION</legend>
             <p>     I hereby declare that the above written particulars are true to the best of myknowledge and belief.</p>
         </fieldset> 
-        <p>DATE:</p><p>PLACE:</p>       
+        <p className='dateplace'>DATE:</p><p className='dateplace'>PLACE:</p>       
 
     
     </section>
